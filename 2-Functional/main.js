52cc75f196c918392ee6aa7fd8c439ac1657cdb6
Pol Carreño <carrenotoralpol@gmail.com>
2025-02-18 13:09:29 +0100

    error codigo

--- 2-Functional/main.js
+++ 2-Functional/main.js
@@ -3,7 +3,7 @@
 
 // Given an array of numbers, double each number and return a new array
 const numbers = [1, 2, 3, 4, 5];
-const doubledNumbers = [];
+let doubledNumbers = [];
 
 // Imperative (Non-Functional) Approach
 for (let i = 0; i < numbers.length; i++) {
@@ -66,7 +66,7 @@ function multiply (a, b) {
 	return a * b;
 }
 
-console.log(aplicarOperacion(3, 4, multiply));  // 12
+console.log(applyOperation(3, 4, multiply));  // 12
 
 
 
